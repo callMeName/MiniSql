d441f4c5d2cbcfa4afe63aff90ad19dc6aff576a
callMeName <rickyxuusa@gmail.com>
2014-11-05 10:08:29 +0800

    BufferManager.h V1.3

--- BufferManager.h
+++ BufferManager.h
@@ -14,31 +14,31 @@
 
 static int replaced_block = -1;
 
+
 class BufferManager
 {
     private:
         fileNode *fileHead;
         fileNode file_pool[MAX_FILE_NUM];
         blockNode block_pool[MAX_BLOCK_NUM];
-        int total_block;
-        int total_file;
-        void init_block(blockNode &block);
-        void init_file(fileNode &file);
+        int total_block; // the number of block that have been used, which means the block is in the list.
+        int total_file; // the number of file that have been used, which means the file is in the list.
+        void init_block(blockNode & block);
+        void init_file(fileNode & file);
         blockNode* getBlock(const char* fileName,blockNode* position,bool if_pin = false);
         void writtenBackToDiskAll();
         void writtenBackToDisk(const char* fileName,blockNode* block);
         void clean_dirty(blockNode &block);
-
     
     public:
         BufferManager();
         ~BufferManager();
         fileNode* getFile(const char* fileName,bool if_pin = false);
-        void set_dirty(blockNode &block);
-        void set_pin(blockNode &block,bool pin);
-        void set_pin(fileNode &file,bool pin);
-        blockNode* getNextBlock(const char * fileName,blockNode* block);
-        blockNode* getBlockHead(const char * fileName,fileNode* file);
+        void set_dirty(blockNode & block);
+        void set_pin(blockNode & block,bool pin);
+        void set_pin(fileNode & file,bool pin);
+        blockNode* getNextBlock(const char* fileName,blockNode* block);
+        blockNode* getBlockHead(const char* fileName,fileNode* file);
 };
 
 #endif /* defined(__Minisql__BufferManager__) */
